dd2fd1cf1dd5dcae77c09b6813a55c7b8d81ae27
Home <shilov.grish@bk.ru>
2022-12-29 23:26:37 +0300

    Code

---
+++ Lab_2/1_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 150;
+    float b = 15.933;
+    short c = 250;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    int day = 02, year = 2003;
+    string month = "июнь";
+    cout << "Моя дата рождения: " << day << month << year << "года." << endl;
+    const float x = 56.458;
+    const string y = "WINDOWS";
+    cout << x << " " << y << endl;
+    return 0;
+}
